f3cc06f540fada9dce0a60d46a71025d0a55657e
tmdgusya <tmdgusya@naver.com>
2021-03-22 15:44:54 +0900

    feat : Url 처리 로직 간단 설계
    - HttpRequest Header 의 첫줄을 분석해서 요청 Url 을 분석할 수 있도록 하였음.

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -6,8 +6,22 @@ import java.util.stream.Collectors;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpRequestUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(HttpRequestUtils.class);
+
+    public static String getUrl(String firstLine) {
+        String[] tokens = firstLine.split(" ");
+        if(tokens.length != 3) {
+            //TODO : RETURN HTTP STATUS 500 ?
+        }
+        return tokens[1];
+    }
+
+
     /**
      * @param queryString은
      *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
