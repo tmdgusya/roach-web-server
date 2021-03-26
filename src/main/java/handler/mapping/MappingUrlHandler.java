f40851feab708b18d130bef999bdb3662c87c0ff
tmdgusya <tmdgusya@naver.com>
2021-03-26 23:41:54 +0900

    [#1] feat: :smiley_cat: 어노테이션을 통한 MappingUrlHandler 를 만든다
    Mapping Annotation 에는 url 을 default 로 사용할 수 있게 하였고, @Contoller 어노테이션은 해당 컨트롤러 디렉토리 및의 클래스 중 @Controller 가 붙어있는 클래스들을 찾아 동적으로 요청을 분석하여 해당 메소드를 리플렉션을 통해 실행시켜줍니다. 하지만 아직까지 controller 밑의 디렉토리에 만들어줘야 한다는 단점이 있습니다. 이는 후에 공부를 더한뒤 보완되야 할듯 합니다.

---
+++ src/main/java/handler/mapping/MappingUrlHandler.java
@@ -0,0 +1,86 @@
+package handler.mapping;
+
+import core.HttpRequest;
+import core.HttpResponse;
+import org.reflections.Reflections;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import static util.ReflectionUtils.*;
+
+public class MappingUrlHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(MappingUrlHandler.class);
+
+    private final String method;
+    private final String url;
+    private String className;
+    private Method controllerMethod;
+
+    public MappingUrlHandler(String method, String url) {
+        this.method = method;
+        this.url = url;
+    }
+
+
+    public void invokeMethod(HttpRequest httpRequest, HttpResponse httpResponse) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
+        final Method method = this.findMethod();
+        final Class<?> targetClass = Class.forName(className);
+        final Object controllerClass = targetClass.getConstructor().newInstance();
+        final Class<?>[] parameterTypes = method.getParameterTypes();
+        method.invoke(controllerClass, httpRequest, httpResponse);
+    }
+
+    private Method findMethod() throws ClassNotFoundException {
+        return findController(scanForControllers("controller"));
+    }
+
+    private Method findController(Class<?>[] controllerclasses) {
+        for(Class c : controllerclasses) {
+            this.className = c.getName();
+            final Method[] declaredMethods = c.getDeclaredMethods();
+            if(method.equals("GET")) {
+                return findGetController(declaredMethods);
+            }
+            if(method.equals("POST")) {
+                return findPostController(declaredMethods);
+            }
+        }
+        throw new IllegalArgumentException("500 을 리턴해야한다.");
+    }
+
+    private Method findGetController(Method[] methods) {
+        for(Method method : methods) {
+            if(method.isAnnotationPresent(GetMapping.class)) {
+                final GetMapping annotation = method.getAnnotation(GetMapping.class);
+                if(annotation.url().equals(this.url)) {
+                    return method;
+                }
+            }
+        }
+        throw new IllegalArgumentException("500 을 리턴해야한다.");
+    }
+
+    private Method findPostController(Method[] methods) {
+        for(Method method : methods) {
+            if(method.isAnnotationPresent(PostMapping.class)){
+                final PostMapping annotation = method.getAnnotation(PostMapping.class);
+                if(annotation.url().equals(this.url)) {
+                    return method;
+                }
+            }
+        }
+        throw new IllegalArgumentException("500 을 리턴해야한다.");
+    }
+
+
+
+
+}
