6db6f6b58dc1010ae486a582fd1e527334d1b682
tmdgusya <tmdgusya@naver.com>
2021-03-27 14:11:39 +0900

    [#5] feat: :smiley_cat: create Cookie Class
    Response 와 Request 에서 공통적으로 사용될 수 있는 Cookie 클래스를 등록하여 정상적으로 등록될 수 있도록 만들었습니다.

---
+++ src/main/java/core/Cookie.java
@@ -0,0 +1,36 @@
+package core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cookie {
+
+    private final String COOKIE = "cookie";
+
+    private final Map<String, String> cookies = new HashMap<>();
+
+    public void saveCookie(String cookie) {
+        String[] cookieEntry = cookie.split("=");
+        for(int i = 0; i < cookieEntry.length; i++) {
+            cookies.put(cookieEntry[0], cookieEntry[1]);
+        }
+    }
+
+    public void addCookie(String key, String value) {
+        cookies.put(key, value);
+    }
+
+    public Map<String, String> getCookies() {
+        return cookies;
+    }
+
+    public String toString() {
+        StringBuilder cookie = new StringBuilder();
+        for(String key : cookies.keySet()) {
+            cookie.append(key).append("=").append(cookies.get(key)).append(", ");
+
+        }
+        return cookie.toString().substring(0, cookie.length()-2);
+    }
+
+}
