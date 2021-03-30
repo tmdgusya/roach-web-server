bcbb5428d87899cfbaaf5d38cf598ac87eb7e8eb
tmdgusya <tmdgusya@naver.com>
2021-03-30 15:43:34 +0900

    [#9] feat: :smiley_cat: ReturnValueHandler 를 만든다.
    String 으로 View 를 Resolve 할 수 있도록 해준다.

--- src/main/java/util/ReflectionUtils.java
+++ src/main/java/util/ReflectionUtils.java
@@ -5,15 +5,21 @@ import org.reflections.Reflections;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class ReflectionUtils {
 
-    public static Class<?>[] scanForControllers(String packageStr) {
+    private static final Logger log = LoggerFactory.getLogger(ReflectionUtils.class);
+
+    public static ArrayList<Class<?>> scanForControllers(String packageStr) {
         Reflections reflections = new Reflections((new ConfigurationBuilder()).setUrls(ClasspathHelper.forPackage(packageStr)).filterInputsBy((new FilterBuilder()).includePackage(packageStr)));
-        Set classes = reflections.getTypesAnnotatedWith(Controller.class);
-        return (Class[])classes.toArray(new Class[classes.size()]);
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        log.info("Controller classes : {}", classes);
+        return new ArrayList<>(classes);
     }
 
 }
--- src/main/java/webserver/RoachCat.java
+++ src/main/java/webserver/RoachCat.java
@@ -2,12 +2,12 @@ package webserver;
 
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.Socket;
 
 import core.HttpRequest;
 import core.HttpResponse;
 import handler.mapping.MappingUrlHandler;
+import handler.returnValueHandle.ReturnValueHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,11 +29,15 @@ public class RoachCat extends Thread {
             httpRequest.run();
             HttpResponse httpResponse = new HttpResponse(out);
             MappingUrlHandler mappingUrlHandler = new MappingUrlHandler(httpRequest.getMethod(), httpRequest.getUrl());
+            DataOutputStream dos = new DataOutputStream(out);
+            if(httpRequest.getUrl().contains(".")) {
+                ReturnValueHandler.responseStaticFile(dos, httpRequest.getUrl());
+                return;
+            }
             try {
                 Object returnValue = mappingUrlHandler.invokeMethod(httpRequest, httpResponse);
-                //TODO 여기서 returnValue 를 resolve 하는 방식을 찾아야 한다.
-                //ReturnValueHandler returnValueHandler = new ReturnValueHandler();
-                //returnValueHandler.handle(returnValue); => 내부적으로 View 로 돌릴 수도 있고, 아니면 JSON 으로 바꿀수도있고 여하튼
+                ReturnValueHandler returnValueHandler = new ReturnValueHandler(returnValue, dos);
+                returnValueHandler.handle();
             } catch (IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException | InstantiationException e) {
                 e.printStackTrace();
             }
