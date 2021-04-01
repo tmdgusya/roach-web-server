6f131f1d7568216dafb2ef5ebdc3e9b16808764d
tmdgusya <tmdgusya@naver.com>
2021-04-01 17:04:22 +0900

    :refactor :recycle: 디렉토리 이동

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,8 +1,10 @@
 package webserver;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import container.BeanFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +18,12 @@ public class WebServer {
             port = DEFAULT_PORT;
         } else {
             port = Integer.parseInt(args[0]);
+        }
+
+        try {
+            BeanFactory beanFactory = new BeanFactory();
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
         }
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
