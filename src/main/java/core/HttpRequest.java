604fbfc0586f8c586ba6840b2578643fc7302fb5
tmdgusya <tmdgusya@naver.com>
2021-04-14 22:49:18 +0900

    [#41] feat: :smiley_cat: HttpRequest 의 책임을 분할한다.
    기존의 HttpRequest 는 Parsing 하는 역할도 지니고 있었는데 이를 분리한다

--- src/main/java/core/HttpRequest.java
+++ src/main/java/core/HttpRequest.java
@@ -20,50 +20,20 @@ public class HttpRequest {
 
     private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
 
-    private InputStream inputStream;
-    private String url;
-    private RequestMethod method;
-    private Header header = new Header();
-    private Cookie cookie;
+    private final String url;
+    private final RequestMethod method;
+    private final Header header;
+    private final Cookie cookie;
     private Map<String, String> parameters;
     private Map<String, String> requestBody;
 
-    public HttpRequest(InputStream inputStream) {
-        this.inputStream = inputStream;
-    }
-
-    public void run() throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-
-        String line = br.readLine();
-
-        if (line == null) {
-            return;
-        }
-
-        final String[] fistLine = line.split(" ");
-
-        this.method = RequestMethod.valueOf(fistLine[0]);
-        this.url = fistLine[1];
-
-        header.saveHeaders(br, line);
-
-        int queryStringStartIndex = url.indexOf("?");
-
-        if(queryStringStartIndex != -1) {
-            parameters = HttpRequestUtils.parseQueryString(url.substring(queryStringStartIndex + 1, url.length()));
-            url = url.substring(0, queryStringStartIndex);
-        }
-
-        if(!method.equals(RequestMethod.GET)) {
-            requestBody = HttpRequestUtils.parseQueryString(IOUtils.readData(br, Integer.parseInt(header.getAttribute("Content-Length"))));
-        }
-
-        if (header.isExistAttribute(COOKIE)) {
-            cookie.saveCookie(header.getAttribute(COOKIE));
-        }
-
+    public HttpRequest(String url, RequestMethod method, Header header, Cookie cookie, Map<String, String> parameters, Map<String, String> requestBody) {
+        this.url = url;
+        this.method = method;
+        this.header = header;
+        this.cookie = cookie;
+        this.parameters = parameters;
+        this.requestBody = requestBody;
     }
 
     public static Logger getLog() {
