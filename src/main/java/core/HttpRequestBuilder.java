f2f91e6de6defaf02a1919873a87a295ab848e3e
tmdgusya <tmdgusya@naver.com>
2021-04-14 22:48:30 +0900

    [#41] feat: :smiley_cat: HttpRequestBuilder 를 생성한다.
    기존의 HttpRequest 를 조금 더 보기쉽게 생성하기 위해 Builder 를 만들어준다.

---
+++ src/main/java/core/HttpRequestBuilder.java
@@ -0,0 +1,48 @@
+package core;
+
+import java.util.Map;
+
+public class HttpRequestBuilder {
+
+    private String url;
+    private RequestMethod method;
+    private final Header header = new Header();
+    private Cookie cookie;
+    private Map<String, String> parameters;
+    private Map<String, String> requestBody;
+
+    public HttpRequestBuilder setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public HttpRequestBuilder setMethod(RequestMethod method) {
+        this.method = method;
+        return this;
+    }
+
+    public HttpRequestBuilder addHeader(String key, String value) {
+        header.addHeader(key, value);
+        return this;
+    }
+
+    public HttpRequestBuilder setCookie(String key, String value) {
+        cookie.addCookie(key, value);
+        return this;
+    }
+
+    public HttpRequestBuilder setParameters(Map<String, String> parameters) {
+        this.parameters = parameters;
+        return this;
+    }
+
+    public HttpRequestBuilder setRequestBody(Map<String,String> requestBody) {
+        this.requestBody = requestBody;
+        return this;
+    }
+
+    public HttpRequest build() {
+        return new HttpRequest(url, method, header, cookie, parameters, requestBody);
+    }
+
+}
